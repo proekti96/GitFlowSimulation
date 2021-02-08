fc9633625faa5e9d1de18fd320f1fd1550bb9845
Daniela Janushevska <janushevska.daniela@students.finki.ukim.mk>
2021-02-08 19:15:57 +0100

    added new user model and initializaed in main

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,19 @@
+import java.lang.Exception
 
 class main {
 
-   fun  main(): Unit = throw NotImplementedError()
+   fun  main(): Unit {
+      var user: UserModel = null!!
+      try {
+         user = UserModel("daniela", "ne-ti-kazuvam", "daniela@janushevska.com")
+         println("The user succesfully initialized")
+      }
+      catch(ex: Exception) {
+         println("User initialization went wrong")
+      }
+      finally {
+         println("Username: ${user.username}")
+         println("Program finished")
+      }
+   }
 }
